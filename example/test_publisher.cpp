5d36864a39dae85bade29373b7b419b31bc7fb85
george-ko88 <RUANKON530@outlook.com>
2024-11-07 15:32:22 +0000

    11.07:修改序列化vector读函数错误

--- example/test_publisher.cpp
+++ example/test_publisher.cpp
@@ -15,36 +15,43 @@ struct TestMsg : public Serializable
     SERIALIZE(timestamp,image)
 };
 
+namespace george
+{
+    void test_rtps_pub(void)
+    {
+        config::RoleAttributes role_attr;
+        role_attr.channel_name = "/chatter0";
+        role_attr.node_name = "publisher";
+        role_attr.channel_id =common::GlobalData::RegisterChannel("/chatter0");
+        
+        std::cout<<boolalpha;
+        int n = 0;
+        TestMsg testmsg;
+        testmsg.timestamp = 0;
+        testmsg.image.resize(1920*1080*3,0);
+
+        //设置发送数据的大小
+        serialize::DataStream ds; 
+        ds << testmsg;
+        role_attr.qos_profile.msg_size= ds.size();
+
+        Publisher<TestMsg> publisher(role_attr);
+        std::cout<<"Init publisher " << publisher.Init()<< std::endl;
+
+        std::shared_ptr<TestMsg> msg_ptr = std::make_shared<TestMsg>(testmsg);
+        while (1)
+        {
+        publisher.Publish(msg_ptr);
+        std::cout << "Publisher seq: " << n << std::endl;
+        std::this_thread::sleep_for(std::chrono::milliseconds(250));
+        n++;
+        }
+    }
+}
 int main()
 {
     Logger_Init("test.log");
-    config::RoleAttributes role_attr;
-    role_attr.channel_name = "/chatter0";
-    role_attr.node_name = "publisher";
-    role_attr.channel_id =common::GlobalData::RegisterChannel("/chatter0");
-    
-    std::cout<<boolalpha;
-    int n = 0;
-    TestMsg testmsg;
-    testmsg.timestamp = 0;
-    testmsg.image.resize(1920*1080*3,0);
-
-    //设置发送数据的大小
-    serialize::DataStream ds; 
-    ds << testmsg;
-    role_attr.qos_profile.msg_size= ds.size();
-
-    Publisher<TestMsg> publisher(role_attr);
-    std::cout<<"Init publisher " << publisher.Init()<< std::endl;
-
-    std::shared_ptr<TestMsg> msg_ptr = std::make_shared<TestMsg>(testmsg);
-    while (1)
-    {
-       publisher.Publish(msg_ptr);
-       std::cout << "Publisher seq: " << n << std::endl;
-       std::this_thread::sleep_for(std::chrono::milliseconds(250));
-       n++;
-    }
+    george::test_rtps_pub();
     
     return 0;
 }
--- example/test_subscriber.cpp
+++ example/test_subscriber.cpp
@@ -3,29 +3,43 @@
 #include <cmw/transport/message/message_info.h>
 using namespace hnu::cmw;
 
+struct TestMsg : public Serializable
+{
+    uint64_t timestamp;  
 
+    std::vector<char> image;
+    SERIALIZE(timestamp,image)
+};
+
+namespace george
+{
+    void test_rtps_sub(void)
+    {
+        config::RoleAttributes role_attr;
+        role_attr.channel_name = "/chatter0";
+        role_attr.node_name = "subscriber";
+        role_attr.channel_id =common::GlobalData::RegisterChannel("/chatter0");
+
+
+        Subscriber<TestMsg> subscriber(
+                role_attr,[](const std::shared_ptr<TestMsg>& msg){
+                    std::cout << "timestamp is "<< msg->timestamp << std::endl;
+                });
+
+        std::boolalpha;
+
+        std::cout<<"Init subscriber " << subscriber.Init() << std::endl;
+
+        while (1)
+        {
+            /* code */
+        }
+    }
+}
 
 int main()
 {
-    config::RoleAttributes role_attr;
-    role_attr.channel_name = "/chatter0";
-    role_attr.node_name = "subscriber";
-    role_attr.channel_id =common::GlobalData::RegisterChannel("/chatter0");
-
-
-    Subscriber<string> subscriber(
-            role_attr,[](const std::shared_ptr<string>& msg){
-                std::cout << *msg << std::endl;
-            });
-
-    std::boolalpha;
-
-    std::cout<<"Init publisher " << subscriber.Init() << std::endl;
-
-    while (1)
-    {
-        /* code */
-    }
+    george::test_rtps_sub();
     
     return 0;
 }
