295ca355e770842e80e4ffaa6adc5039aadff43d
george-ko88 <RUANKON530@outlook.com>
2024-11-07 14:54:00 +0000

    11.07:修复vector类型序列化读函数问题

--- example/test_subscriber.cpp
+++ example/test_subscriber.cpp
@@ -3,29 +3,36 @@
 #include <cmw/transport/message/message_info.h>
 using namespace hnu::cmw;
 
+namespace george
+{
+    void test_rtsp_sub(void)
+    {
+        config::RoleAttributes role_attr;
+        role_attr.channel_name = "/chatter0";
+        role_attr.node_name = "subscriber";
+        role_attr.channel_id =common::GlobalData::RegisterChannel("/chatter0");
+
+
+        Subscriber<string> subscriber(
+                role_attr,[](const std::shared_ptr<string>& msg){
+                    std::cout << *msg << std::endl;
+                });
+
+        std::boolalpha;
+
+        std::cout<<"Init publisher " << subscriber.Init() << std::endl;
+
+        while (1)
+        {
+            /* code */
+        }
+    }
+}
 
 
 int main()
 {
-    config::RoleAttributes role_attr;
-    role_attr.channel_name = "/chatter0";
-    role_attr.node_name = "subscriber";
-    role_attr.channel_id =common::GlobalData::RegisterChannel("/chatter0");
 
-
-    Subscriber<string> subscriber(
-            role_attr,[](const std::shared_ptr<string>& msg){
-                std::cout << *msg << std::endl;
-            });
-
-    std::boolalpha;
-
-    std::cout<<"Init publisher " << subscriber.Init() << std::endl;
-
-    while (1)
-    {
-        /* code */
-    }
     
     return 0;
 }
